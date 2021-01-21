1b9890ef797d1d543b13c599b0abf9434d27ace6
alkoumi <komy_66@hotmail.com>
2021-01-21 16:42:18 +0300

    Add auto discover

--- src/ShamelServiceProvider.php
+++ src/ShamelServiceProvider.php
@@ -10,6 +10,16 @@ class ShamelServiceProvider extends ServiceProvider
 {
 
     /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        AliasLoader::getInstance()->alias('Shamel', Shamel::class);
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
@@ -20,35 +30,25 @@ class ShamelServiceProvider extends ServiceProvider
         $this->mergeConfigFrom($config, 'shamelsms');
         $this->publishes([__DIR__ . '/../config/shamelsms.php' => config_path('shamelsms.php')], 'config');
 
-
-        // $this->app->singleton('shamel', function () {
-        //     return $this->app->make('Alkoumi\LaravelShamelSms\ShamelSMS');
-        // });
+         $this->app->singleton('shamel', function () {
+             return $this->app->make('Alkoumi\LaravelShamelSms\ShamelSMS');
+         });
 
         // $this->app->alias(Shamel::class, 'Shamel');
 
         //call sentry's service provider
-        $this->app->register(ShamelServiceProvider::class);
+        //$this->app->register(ShamelServiceProvider::class);
 
         //set up facade
-        AliasLoader::getInstance()->alias('Shamel', Shamel::class);
+        //AliasLoader::getInstance()->alias('Shamel', Shamel::class);
 
         //bind mypackage class
-        $this->app->bind('shamel', function () {
-            return new ShamelSMS();
-        });
-        
+//        $this->app->bind('shamel', function () {
+//            return new ShamelSMS();
+//        });
+
 
     }
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-
-    }
 
 }
